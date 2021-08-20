7d59a28796e170d0bf8305624a720fbfe5b29e0a
HT <hetaoconquer@gmail.com>
2021-08-20 16:14:06 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -9,7 +9,7 @@ from netmf import netmf_large
 import copy
 from predict import predict_cv
 g = load_dataset('.data_path')
-A, X, z = g['A'], g['X'], g['z']
-model = UDAH(A=A, X=X, sim=A.toarray(),z=z,L=128, K=1,verbose=True, p_val=0.1, p_test=0.1, p_nodes=0.0)
+A, X, l = g['A'], g['X'], g['z']
+model = UDAH(A=A, X=X, sim=A.toarray(),z=l,L=128, K=1,verbose=True)
 sess = model.train('0')
 
