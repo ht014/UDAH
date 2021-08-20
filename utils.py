e244350c5c5bf8b44ce1520f1b4706ecfb7c0137
HT <hetaoconquer@gmail.com>
2021-08-20 16:15:56 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,319 @@
+import numpy as np
+import scipy.sparse as sp
+import warnings
+import itertools
+
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import roc_auc_score, average_precision_score, f1_score
+from sklearn.preprocessing import normalize
+
+
+def edges_to_sparse(edges, N, values=None):
+
+    if values is None:
+        values = np.ones(edges.shape[0])
+
+    return sp.coo_matrix((values, (edges[:, 0], edges[:, 1])), shape=(N, N)).tocsr()
+
+
+def train_val_test_split_adjacency(A, p_val=0.10, p_test=0.05, seed=0, neg_mul=1,
+                                   every_node=True, connected=False, undirected=False,
+                                   use_edge_cover=True, set_ops=True, asserts=False):
+
+    if undirected:
+        assert is_undirected  # make sure is directed
+        A = sp.tril(A).tocsr()  # consider only upper triangular
+        A.eliminate_zeros()
+    else:
+        if is_undirected:
+            warnings.warn('Graph appears to be undirected. Did you forgot to set undirected=True?')
+
+    np.random.seed(seed)
+
+    E = A.nnz
+    N = A.shape[0]
+    s_train = int(E * (1 - p_val - p_test))
+
+    idx = np.arange(N)
+
+    if every_node:
+        if connected:
+            assert sp.csgraph.connected_components(A)[0] == 1  # make sure original graph is connected
+            A_hold = sp.csgraph.minimum_spanning_tree(A)
+        else:
+            A.eliminate_zeros()
+            d = A.sum(1).A1
+
+            if use_edge_cover:
+                hold_edges = edge_cover(A)
+
+                min_size = hold_edges.shape[0]
+                if min_size > s_train:
+                    raise ValueError('Training percentage too low to guarantee every node. Min train size needed {:.2f}'
+                                     .format(min_size / E))
+            else:
+                if N > s_train:
+                    raise ValueError('Training percentage too low to guarantee every node. Min train size needed {:.2f}'
+                                     .format(N / E))
+
+                hold_edges_d1 = np.column_stack(
+                    (idx[d > 0], np.row_stack(map(np.random.choice, A[d > 0].tolil().rows))))
+
+                if np.any(d == 0):
+                    hold_edges_d0 = np.column_stack((np.row_stack(map(np.random.choice, A[:, d == 0].T.tolil().rows)),
+                                                     idx[d == 0]))
+                    hold_edges = np.row_stack((hold_edges_d0, hold_edges_d1))
+                else:
+                    hold_edges = hold_edges_d1
+
+            if asserts:
+                assert np.all(A[hold_edges[:, 0], hold_edges[:, 1]])
+                assert len(np.unique(hold_edges.flatten())) == N
+
+            A_hold = edges_to_sparse(hold_edges, N)
+
+        A_hold[A_hold > 1] = 1
+        A_hold.eliminate_zeros()
+        A_sample = A - A_hold
+
+        s_train = s_train - A_hold.nnz
+    else:
+        A_sample = A
+
+    idx_ones = np.random.permutation(A_sample.nnz)
+    ones = np.column_stack(A_sample.nonzero())
+    train_ones = ones[idx_ones[:s_train]]
+    test_ones = ones[idx_ones[s_train:]]
+
+    # return back the held edges
+    if every_node:
+        train_ones = np.row_stack((train_ones, np.column_stack(A_hold.nonzero())))
+
+    n_test = len(test_ones) * neg_mul
+    if set_ops:
+        if undirected:
+            random_sample = np.random.randint(0, N, [int(2.3 * n_test), 2])
+            random_sample = random_sample[random_sample[:, 0] > random_sample[:, 1]]
+        else:
+            random_sample = np.random.randint(0, N, [int(1.3 * n_test), 2])
+            random_sample = random_sample[random_sample[:, 0] != random_sample[:, 1]]
+
+        random_sample = random_sample[A[random_sample[:, 0], random_sample[:, 1]].A1 == 0]
+        random_sample = random_sample[np.unique(random_sample[:, 0] * N + random_sample[:, 1], return_index=True)[1]]
+        test_zeros = np.row_stack(random_sample)[:n_test]
+        assert test_zeros.shape[0] == n_test
+    else:
+        test_zeros = []
+        while len(test_zeros) < n_test:
+            i, j = np.random.randint(0, N, 2)
+            if A[i, j] == 0 and (not undirected or i > j) and (i, j) not in test_zeros:
+                test_zeros.append((i, j))
+        test_zeros = np.array(test_zeros)
+
+    # split the test set into validation and test set
+    s_val_ones = int(len(test_ones) * p_val / (p_val + p_test))
+    s_val_zeros = int(len(test_zeros) * p_val / (p_val + p_test))
+
+    val_ones = test_ones[:s_val_ones]
+    test_ones = test_ones[s_val_ones:]
+
+    val_zeros = test_zeros[:s_val_zeros]
+    test_zeros = test_zeros[s_val_zeros:]
+
+    if undirected:
+        symmetrize = lambda x: np.row_stack((x, np.column_stack((x[:, 1], x[:, 0]))))
+        train_ones = symmetrize(train_ones)
+        val_ones = symmetrize(val_ones)
+        val_zeros = symmetrize(val_zeros)
+        test_ones = symmetrize(test_ones)
+        test_zeros = symmetrize(test_zeros)
+        A = A.maximum(A.T)
+
+    return train_ones, val_ones, val_zeros, test_ones, test_zeros
+
+
+def sparse_feeder(M):
+    M = sp.coo_matrix(M)
+    return np.vstack((M.row, M.col)).T, M.data, M.shape
+
+
+def cartesian_product(x, y):
+    return np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+
+
+def score_link_prediction(labels, scores):
+    return roc_auc_score(labels, scores), average_precision_score(labels, scores)
+
+
+def score_node_classification(features, z, p_labeled=0.1, n_repeat=10, norm=False):
+    lrcv = LogisticRegressionCV()
+
+    if norm:
+        features = normalize(features)
+
+    trace = []
+    for seed in range(n_repeat):
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=1 - p_labeled, random_state=seed)
+        split_train, split_test = next(sss.split(features, z))
+
+        lrcv.fit(features[split_train], z[split_train])
+        predicted = lrcv.predict(features[split_test])
+
+        f1_micro = f1_score(z[split_test], predicted, average='micro')
+        f1_macro = f1_score(z[split_test], predicted, average='macro')
+
+        trace.append((f1_micro, f1_macro))
+
+    return np.array(trace).mean(0)
+
+
+def get_hops(A, K):
+    hops = {1: A.tolil(), -1: A.tolil()}
+    hops[1].setdiag(0)
+
+    for h in range(2, K + 1):
+        # compute the next ring
+        next_hop = hops[h - 1].dot(A)
+        next_hop[next_hop > 0] = 1
+
+        # make sure that we exclude visited n/edges
+        for prev_h in range(1, h):
+            next_hop -= next_hop.multiply(hops[prev_h])
+
+        next_hop = next_hop.tolil()
+        next_hop.setdiag(0)
+
+        hops[h] = next_hop
+        hops[-1] += next_hop
+
+    return hops
+
+
+def sample_last_hop(A, nodes):
+    N = A.shape[0]
+
+    sampled = np.random.randint(0, N, len(nodes))
+
+    nnz = A[nodes, sampled].nonzero()[1]
+    while len(nnz) != 0:
+        new_sample = np.random.randint(0, N, len(nnz))
+        sampled[nnz] = new_sample
+        nnz = A[nnz, new_sample].nonzero()[1]
+
+    return sampled
+
+
+def sample_all_hops(hops, nodes=None):
+    N = hops[1].shape[0]
+
+    if nodes is None:
+        nodes = np.arange(N)
+
+    return np.vstack((nodes,
+                      np.array([[-1 if len(x) == 0 else np.random.choice(x) for x in hops[h].rows[nodes]]
+                                for h in hops.keys() if h != -1]),
+                      sample_last_hop(hops[-1], nodes)
+                      )).T
+
+
+def to_triplets(sampled_hops, scale_terms):
+    triplets = []
+    triplet_scale_terms = []
+
+    for i, j in itertools.combinations(np.arange(1, sampled_hops.shape[1]), 2):
+        triplet = sampled_hops[:, [0] + [i, j]]
+        triplet = triplet[(triplet[:, 1] != -1) & (triplet[:, 2] != -1)]
+        triplet = triplet[(triplet[:, 0] != triplet[:, 1]) & (triplet[:, 0] != triplet[:, 2])]
+        triplets.append(triplet)
+
+        triplet_scale_terms.append(scale_terms[i][triplet[:, 1]] * scale_terms[j][triplet[:, 2]])
+
+    return np.row_stack(triplets), np.concatenate(triplet_scale_terms)
+
+
+def load_dataset(file_name):
+    if not file_name.endswith('.npz'):
+        file_name += '.npz'
+    with np.load(file_name) as loader:
+        loader = dict(loader)
+        A = sp.csr_matrix((loader['adj_data'], loader['adj_indices'],
+                           loader['adj_indptr']), shape=loader['adj_shape'])
+
+        X = sp.csr_matrix((loader['attr_data'], loader['attr_indices'],
+                           loader['attr_indptr']), shape=loader['attr_shape'])
+
+        z = loader.get('labels')
+
+        graph = {
+            'A': A,
+            'X': X,
+            'z': z
+        }
+
+        idx_to_node = loader.get('idx_to_node')
+        if idx_to_node:
+            idx_to_node = idx_to_node.tolist()
+            graph['idx_to_node'] = idx_to_node
+
+        idx_to_attr = loader.get('idx_to_attr')
+        if idx_to_attr:
+            idx_to_attr = idx_to_attr.tolist()
+            graph['idx_to_attr'] = idx_to_attr
+
+        idx_to_class = loader.get('idx_to_class')
+        if idx_to_class:
+            idx_to_class = idx_to_class.tolist()
+            graph['idx_to_class'] = idx_to_class
+
+        return graph
+
+
+def edge_cover(A):
+    N = A.shape[0]
+    d_in = A.sum(0).A1
+    d_out = A.sum(1).A1
+
+    # make sure to include singleton nodes (nodes with one incoming or one outgoing edge)
+    one_in = np.where((d_in == 1) & (d_out == 0))[0]
+    one_out = np.where((d_in == 0) & (d_out == 1))[0]
+
+    edges = []
+    edges.append(np.column_stack((A[:, one_in].argmax(0).A1, one_in)))
+    edges.append(np.column_stack((one_out, A[one_out].argmax(1).A1)))
+    edges = np.row_stack(edges)
+
+    edge_cover_set = set(map(tuple, edges))
+    nodes = set(edges.flatten())
+
+    # greedly add other edges such that both end-point are not yet in the edge_cover_set
+    cands = np.column_stack(A.nonzero())
+    for u, v in cands[d_in[cands[:, 1]].argsort()]:
+        if u not in nodes and v not in nodes and u != v:
+            edge_cover_set.add((u, v))
+            nodes.add(u)
+            nodes.add(v)
+        if len(nodes) == N:
+            break
+
+    # add a single edge for the rest of the nodes not covered so far
+    not_covered = np.setdiff1d(np.arange(N), list(nodes))
+    edges = [list(edge_cover_set)]
+    not_covered_out = not_covered[d_out[not_covered] > 0]
+
+    if len(not_covered_out) > 0:
+        edges.append(np.column_stack((not_covered_out, A[not_covered_out].argmax(1).A1)))
+
+    not_covered_in = not_covered[d_out[not_covered] == 0]
+    if len(not_covered_in) > 0:
+        edges.append(np.column_stack((A[:, not_covered_in].argmax(0).A1, not_covered_in)))
+
+    edges = np.row_stack(edges)
+
+    # make sure that we've indeed computed an edge_cover
+    assert A[edges[:, 0], edges[:, 1]].sum() == len(edges)
+    assert len(set(map(tuple, edges))) == len(edges)
+    assert len(np.unique(edges)) == N
+
+    return edges
+
